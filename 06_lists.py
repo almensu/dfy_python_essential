56a544a26c923f1428e6179afd2bd70d5711f995
almensu <yanghoo205@hotmail.com>
2018-01-04 16:06:26 +0800

    tuple

--- 06_lists.py
+++ 06_lists.py
@@ -86,3 +86,16 @@ print(L[60::2])
 
 
 # 元祖 小括号，起到保护机制
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+# 打印Apple:
+print(L[0][0])
+# 打印Python:
+print(L[1][1])
+# 打印Lisa:
+print(L[2][2])
