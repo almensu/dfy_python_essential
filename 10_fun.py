d986b56a079c1c3cbd3a8b928e4964754202f024
almensu <yanghoo205@hotmail.com>
2018-01-09 15:57:27 +0800

    all

---
+++ 10_fun.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# def print_hello():
+#     print('Hello')
+
+# print_hello()
+
+# def print_hello(name):
+#     print('Hello, ' + name + '!')
+#
+# print_hello('Luo')
+#
+#
+# def sum(x,y,z):
+#     result = x + y + z
+#     return result
+#
+# r = sum(10,3,8)
+# print(r)
+
+def print_lists(names):
+    for name in names:
+        print(name)
+
+dict = ['li','luo','Wu','Du']
+
+print_lists(dict)
--- 11_class.py
+++ 11_class.py
@@ -1,29 +1,55 @@
 # -*- coding: utf-8 -*-
 
-class Dog():
-    def __init__(self, name, age, weight=10):
+# class Dog():
+#     def __init__(self, name, age, weight=10):
+#         self.name = name
+#         self.age = age
+#         self.weight = weight
+#
+#     def eat_bone(self):
+#         print(self.name + ' eatting bone!')
+#
+#     def bark(self):
+#         print(self.name + ' is barking!')
+#
+#     def set_weight(self, weight):
+#         self.weight = weight
+#
+#
+#
+# tudou = Dog('Tudou', '3', weight=15)
+#
+# print(tudou.name)
+# print(tudou.age)
+# print(tudou.weight)
+#
+# tudou.eat_bone()
+# tudou.bark()
+# tudou.set_weight(999)
+# print(tudou.weight)
+
+
+class Student(object):
+
+    def __init__(self, name, score):
         self.name = name
-        self.age = age
-        self.weight = weight
+        self.score = score
 
-    def eat_bone(self):
-        print(self.name + ' eatting bone!')
+    def print_score(self):
+        print('%s:%s' % (self.name, self.score))
 
-    def bark(self):
-        print(self.name + ' is barking!')
+    def get_grade(self):
+        if self.score >=90:
+            return 'A'
+        elif self.score >=60:
+            return 'B'
+        else:
+            return 'C'
 
-    def set_weight(self, weight):
-        self.weight = weight
+luo = Student('MX Luo', 80)
+su = Student('YH Su', 90)
 
+print(luo.print_score())
+print(su.print_score())
 
-
-tudou = Dog('Tudou', '3', weight=15)
-
-print(tudou.name)
-print(tudou.age)
-print(tudou.weight)
-
-tudou.eat_bone()
-tudou.bark()
-tudou.set_weight(999)
-print(tudou.weight)
+print('Grade of Luo: ' + luo.get_grade())
