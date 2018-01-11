a8cee40a03b1ab8928dc02bca4a091136b889cb3
almensu <yanghoo205@hotmail.com>
2018-01-11 20:28:44 +0800

    add

--- 13_if_while_for.py
+++ 13_if_while_for.py
@@ -1,3 +1,5 @@
+
+#----------------------------
 # 第一题：利用条件运算符的嵌套来完成此题：
 # 学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。
 
@@ -11,6 +13,8 @@
 #     else scores >= 0 and scores <= 60:
 #         return 'C'
 
+
+#----------------------------
 # 我做的题2
 # score = int(input('输入你的分数：'))
 # if score >= 90:
@@ -29,7 +33,7 @@
 # else:
 #     print('你的成绩为：C')
 
-
+#----------------------------
 # 第二题：for
 #     创建一个名为favorite_places的字典。
 #     在这个字典中，将三个人的名字用作键；对于其中的每个人，都存储他喜欢的1〜3个地方.
@@ -54,6 +58,7 @@
 #         break
 
 
+#----------------------------
 # 第三题：
 # 99乘法表
 #    用for循环打印99乘法表
@@ -68,6 +73,7 @@
 #         print('\n')
 
 
+#----------------------------
 # 第四题：
 # 1-100的和
 #     1+2+3+4+...+100 = ?
@@ -79,6 +85,7 @@
 #     print(result)
 
 
+#----------------------------
 # 第五题：
 # 从键盘输入一个字符串，将小写字母全部转换成大写字母,
 #     将字符串以列表的形式输出(如果字符串包含整数,转为整型)?
@@ -93,34 +100,90 @@
 #         dict1.append(str1.upper())
 # print(dict1)
 
+
+#----------------------------
 # 第六题：
 # 随机输入8位以内的的正整数，要求：一、求它是几位数，二、逆序打印出各位数字。
 
-nums = input('请随机输入8位以内的的正整数： ')
-str1 = ''
-if len(nums) <= 8:
-    int1 = int(len(nums))
-    print('它是', int1, '位数。')
-    # 或者这样拼接
-    print('它是{0}位数'.format(int1))
-
-    # 方法一
-    for char in list(reversed(nums)):
-        str1 += char
-    print(str1)
-
-    # 方法二
-    char_reversed = nums[::-1]
-    print('逆序数字是：{0}'.format(char_reversed))
-
-else:
-    print('请输入8位以内的的正整数！！')
-
-
+# nums = input('请随机输入8位以内的的正整数： ')
+# str1 = ''
+# if len(nums) <= 8:
+#     int1 = int(len(nums))
+#     print('它是', int1, '位数。')
+#     # 或者这样拼接
+#     print('它是{0}位数'.format(int1))
+#
+#     # 方法一
+#     for char in list(reversed(nums)):
+#         str1 += char
+#     print(str1)
+#
+#     # 方法二
+#     char_reversed = nums[::-1]
+#     print('逆序数字是：{0}'.format(char_reversed))
+#
+# else:
+#     print('请输入8位以内的的正整数！！')
 
 
+#----------------------------
+# 第七题：
+# 一球从n米(自己输入)高度自由落下，每次落地后反跳回原高度的一半；
+#     再落下，求它在第10次落地时，共经过多少米？
 
 
 
+#----------------------------
+# 第八题：
+# 输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
 
+# str = input('请输入一段文字：')
+#
+# letter = 0
+# space = 0
+# numeric = 0
+# other = 0
+#
+# for char in str:
+#     if char.isalpha():
+#         letter += 1
+#     elif char.isspace():
+#         space += 1
+#     elif char.isnumeric():
+#         numeric += 1
+#     else:
+#         other += 1
+#
+# print('输入的字符串中，字母{0}个，空格{1}个，数字{2}个，其他字符{3}个'.format(letter, space, numeric, other))
+
+
+#----------------------------
+# 第九题：
+# names = ['Tom','Billy','Jefferson','Andrew','Wesley','Steven',
+# 'Joe','Alice','Jill','Ana','Wendy','Jennifer','Sherry','Eva']
+# 找出上述名字中长度大于4的名字,组成列表打印出来.
+# 过滤掉长度大于5的字符串列表，并将剩下的转换成大写字母.
+
+# names = ['Tom','Billy','Jefferson','Andrew','Wesley','Steven',
+# 'Joe','Alice','Jill','Ana','Wendy','Jennifer','Sherry','Eva']
+#
+# names4 = []
+# names5 = []
+#
+# for n in names:
+#     if len(n) > 4:
+#         names4.append(n)
+#
+# for n in names:
+#     if len(n) <= 5:
+#         # n = n.upper()
+#         names5.append(n.upper())
+#
+#
+# print('长度大于4的名字: ', names4)
+# print('剩下的转换成大写字母: ', names5)
+
+
+
+#----------------------------
 
